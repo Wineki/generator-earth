4ba8039b2a9c79f82c94627cfaedef662e61c853
cxw <caoxuewei_jinan@163.com>
2019-06-04 20:49:05 +0800

    init

---
+++ project/react-latest-ts/src/pages/index/containers/Site/list.tsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+import ListItem from '../../components/ListItem/index'
+
+//长页面滚动 位置记录
+let scrollSite = 0;
+
+/**
+ * 定义组件props
+ */
+interface Props {
+    listData: object[]
+}
+
+interface ListItemI {
+    id: string,
+    title: string
+}
+class List extends Component<Props> {
+    constructor(props: Props) {
+        super(props);
+    }
+    props: Props;
+    componentDidMount () {
+
+        //回到之前浏览的位置
+        window.scrollTo(0, scrollSite);
+
+    }
+
+    componentWillUnmount () {
+
+        //长页面滚动 位置记录
+        scrollSite = window.scrollY;
+
+    }
+
+    render () {
+
+        const { listData } = this.props;
+
+        return (
+
+            <div>
+
+                {
+
+                    listData.map((item: ListItemI):any => {
+
+                        return <ListItem key={item.id} title={item.title} id={item.id}/>
+
+                    })
+
+                }
+
+            </div>
+
+        )
+
+    }
+
+}
+
+export default List;
