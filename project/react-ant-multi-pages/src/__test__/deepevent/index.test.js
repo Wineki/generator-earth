e720c1aff08e2ae0a8d33f834f93c816133192f4
zuoguangcheng <zuoguangcheng@58.com>
2019-09-18 17:16:13 +0800

    feat: add unit-test-demo

---
+++ project/react-ant-multi-pages/src/__test__/deepevent/index.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { mount, render, shallow } from 'enzyme';
+import Deep from '../component/deep/index.js';
+
+describe('deep', () => {
+  it('测试mount 触发事件', () => {
+    const onClick = jest.fn();
+    const wrapper = mount(<Deep onClick={onClick}/>);
+
+    expect(wrapper.find('.index')).toHaveLength(1)
+
+    wrapper.find('.index').invoke('onClick')();
+    expect(onClick).toHaveBeenCalled();
+	});
+
+  it('测试shallow 触发事件', () => {
+    const onClick = jest.fn();
+    const wrapper = shallow(<Deep onClick={onClick}/>);
+
+    wrapper.find('.index').invoke('onClick')();
+    expect(onClick).toHaveBeenCalled();
+  });
+
+  it('测试render 触发事件', () => {
+    const onClick = jest.fn();
+    const wrapper = render(<Deep onClick={onClick}/>);
+
+    wrapper.find('.index').invoke('onClick')();
+    expect(onClick).toHaveBeenCalled(1);
+  });
+})
---
+++ project/react-ant-ts/src/__test__/ty/fun.test.js
@@ -0,0 +1,9 @@
+import fun from '../component/ty/fun';
+
+describe('test', () => {
+  it('111,', () => {
+    console.log(fun('namename', 19));
+
+    // expect(fun('namename', 19))
+  })
+})
